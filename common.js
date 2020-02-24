af18dd6bc2bb138c2773fdf4ce8c86537ed22cad
AA <angelangelov97@gmail.com>
2020-02-24 17:40:02 +0200

    changed web3 with web3connect, fixed responsive design, using Promise.all for stats box

--- common.js
+++ common.js
@@ -68,7 +68,7 @@ async function ensure_token_allowance() {
 
 async function init_contracts() {
     web3.eth.net.getId((err, result) => {
-        if (result == 1) {
+/*        if (result == 1) {
             if (web3.currentProvider.constructor.name == 's') {
                 $('#error-window').text('Error: please use Metamask to do transactions');
                 $('#error-window').show();
@@ -76,8 +76,8 @@ async function init_contracts() {
             else
                 $('#error-window').hide();
         }
-        else
-        {
+        else*/
+        if(result != 1) {
             $('#error-window').text('Error: wrong network type. Please switch to mainnet');
             $('#error-window').show();
         }
@@ -121,11 +121,19 @@ async function update_fee_info() {
     var bal_info = $('#balances-info li span');
     await update_rates();
     var total = 0;
+    var promises = [];
     for (let i = 0; i < N_COINS; i++) {
-        balances[i] = parseInt(await swap.methods.balances(i).call());
+        promises.push(swap.methods.balances(i).call())
+/*        balances[i] = parseInt(await swap.methods.balances(i).call());
+        $(bal_info[i]).text((balances[i] * c_rates[i]).toFixed(2));
+        total += balances[i] * c_rates[i];*/
+    }
+    let resolves = await Promise.all(promises)
+    resolves.forEach((balance, i) => {
+        balances[i] = +balance;
         $(bal_info[i]).text((balances[i] * c_rates[i]).toFixed(2));
         total += balances[i] * c_rates[i];
-    }
+    })
     $(bal_info[N_COINS]).text(total.toFixed(2));
     fee = parseInt(await swap.methods.fee().call()) / 1e10;
     admin_fee = parseInt(await swap.methods.admin_fee().call()) / 1e10;
--- swap.js
+++ swap.js
@@ -120,13 +120,49 @@ async function init_ui() {
 window.addEventListener('load', async () => {
     init_menu();
 
-    if (window.ethereum)
+    const WalletConnectProvider = window.WalletConnectProvider.default
+    const providerOptions = {
+        walletconnect: {
+            package: WalletConnectProvider, // required
+            options: {
+              infuraId: "c334bb4b45a444979057f0fb8a0c9d1b" // required
+            }
+        }
+    };
+
+    const web3Connect = new Web3Connect.default.Core({
+      network: "mainnet", // optional
+      cacheProvider: true, // optional
+      providerOptions // required
+    });
+
+    const provider = await web3Connect.connect();
+
+    provider.on("chainChanged", (chainId) => {
+        console.log(chainId, "CHAIN")
+        if(chainId != 1) {
+            $('#error-window').text('Error: wrong network type. Please switch to mainnet');
+            $('#error-window').show();
+        }
+    });
+
+    provider.on("accountsChanged", (accounts) => {
+        console.log(accounts)
+        location.reload()
+    })
+
+    const web3 = new Web3(provider);
+
+    window.web3 = web3
+
+
+/*    if (window.ethereum)
     {
         window.web3 = new Web3(ethereum);
         await ethereum.enable();
     }
     else
-        window.web3 = new Web3(infura_url);
+        window.web3 = new Web3(infura_url);*/
     await init_contracts();
     await init_ui();
 });
