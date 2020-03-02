bd8c71e98215cac06d2f25710e94b4f95c8c67d1
AA <angelangelov97@gmail.com>
2020-03-02 15:01:18 +0200

    port changes from pcompound2

--- common.js
+++ common.js
@@ -29,7 +29,6 @@ async function ensure_allowance(amounts) {
     if (amounts) {
         // Non-infinite
         for (let i=0; i < N_COINS; i++) {
-            console.log(i, allowances[i], amounts[i]);
             if (allowances[i] < amounts[i]) {
                 if (allowances[i] > 0)
                     await approve(coins[i], 0, default_account);
@@ -173,12 +172,12 @@ async function update_fee_info() {
 async function calc_slippage(deposit) {
     var real_values = [...$("[id^=currency_]")].map((x,i) => +($(x).val()));
     var values = real_values.map((x,i) => BigInt(Math.floor(x / c_rates[i])).toString());
-    var token_amount = await swap.methods.calc_token_amount(values, deposit).call();  // XXX true for deposits, false for withdrawals
+    var token_amount = await swap.methods.calc_token_amount(values, deposit).call();
     var token_supply = parseInt(await swap_token.methods.totalSupply().call());
     let slippage = 0;
     for(let i = 0; i < N_COINS; i++) {
         let coin_balance = parseInt(await swap.methods.balances(i).call()) * c_rates[i];
-        if(!deposit) {            
+        if(!deposit) {
             if(coin_balance < real_values[i]) {
                 $("#nobalance-warning").show();
                 $("#nobalance-warning span").text($("label[for='currency_"+i+"']").text());
@@ -186,12 +185,14 @@ async function calc_slippage(deposit) {
             else
                 $("#nobalance-warning").hide();
         }
-        let balance =  coin_balance * token_amount / token_supply
+        let balance = coin_balance * token_amount / token_supply
         slippage += balance
     }
-    slippage /= real_values.reduce((a,b) => a+b, 0);
+    if (deposit)
+        slippage /= real_values.reduce((a,b) => a+b, 0)
+    else
+        slippage = real_values.reduce((a,b) => a+b, 0) / slippage;
     slippage = 1 - slippage;
-    console.log(slippage);
     if(slippage > 0.005) {
         $("#highslippage-warning").show();
         $("#highslippage-warning span").text(slippage * 100);
