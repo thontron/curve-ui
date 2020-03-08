9a48384d24af5b39eafde8a22e0aabebb8716574
AA <angelangelov97@gmail.com>
2020-03-08 22:18:32 +0200

    small fix

--- deposit.js
+++ deposit.js
@@ -120,18 +120,20 @@ window.addEventListener('DOMContentLoaded', async () => {
         $("#from_currency").attr('disabled', false)
 
     }
-    console.error(err)
-    if(err.reason == 'cancelDialog') {
-        const web3 = new Web3(infura_url);
-        window.web3 = web3
+    catch(err) {    
+        console.error(err)
+        if(err.reason == 'cancelDialog') {
+            const web3 = new Web3(infura_url);
+            window.web3 = web3
 
-        await init_contracts();
-        update_fee_info();
-        await handle_sync_balances();
-        await calc_slippage(true);
+            await init_contracts();
+            update_fee_info();
+            await handle_sync_balances();
+            await calc_slippage(true);
 
-        await init_ui();
-        $("#from_currency").attr('disabled', false)
+            await init_ui();
+            $("#from_currency").attr('disabled', false)
+        }
     }
 
 });
