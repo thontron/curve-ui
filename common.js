5e0f7cbd993727155b9a5e23db58c52ad723fd39
AA <angelangelov97@gmail.com>
2020-03-14 18:58:03 +0200

    wait for checkboxes at deposit and bump versions

--- common.js
+++ common.js
@@ -9,7 +9,7 @@ var c_rates = new Array(N_COINS);
 var fee;
 var admin_fee;
 
-var version = 5;
+var version = 6;
 localStorage.setItem('bUSDversion', version)
 
 var cBN = (val) => new BigNumber(val);
--- deposit.js
+++ deposit.js
@@ -112,6 +112,9 @@ async function init_ui() {
 
 window.addEventListener('DOMContentLoaded', async () => {
     try {
+        $("#max-balances").prop('disabled', true);
+        $("#add-liquidity").prop('disabled', true);
+
         await init();
         update_fee_info();
         await handle_sync_balances();
@@ -119,8 +122,8 @@ window.addEventListener('DOMContentLoaded', async () => {
         
         await init_ui();
 
-        $("#from_currency").attr('disabled', false)
-
+        $("#max-balances").prop('disabled', false);
+        $("#add-liquidity").prop('disabled', false);
     }
     catch(err) {
         if(err.reason == 'cancelDialog') {
@@ -133,7 +136,6 @@ window.addEventListener('DOMContentLoaded', async () => {
             await calc_slippage(true);
 
             await init_ui();
-            $("#from_currency").attr('disabled', false)
         }
     }
 });
