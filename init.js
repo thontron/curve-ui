f95e8c193cce188aef6e2627aff91a29fb4971d8
AA <angelangelov97@gmail.com>
2020-03-14 18:28:09 +0200

    check if web3 is actually web3 and not metamask injected web3

--- init.js
+++ init.js
@@ -1,8 +1,9 @@
 let cancelablePromise;
 $(document).click(function(event) { 
   $target = $(event.target);
+  console.log(web3)
   if(!$target.closest('.web3connect-provider-wrapper').length && 
-  $('.web3connect-provider-wrapper').is(":visible") && cancelablePromise && !window.web3) {
+  $('.web3connect-provider-wrapper').is(":visible") && cancelablePromise && window.web3.version != '1.0.0-beta.34') {
     cancelablePromise.cancel('cancelDialog');
   }        
 });
