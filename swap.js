3c3d4f6a1bf4c959c18a785f38dc8bbb38f39a38
AA <angelangelov97@gmail.com>
2020-03-16 10:26:07 +0200

    fix rounding issues

--- swap.js
+++ swap.js
@@ -67,7 +67,7 @@ function setAmountPromise() {
         if (b >= 0.001) {
             // In c-units
             var dx_ = $('#from_currency').val();
-            var dx = cBN(Math.round(dx_ * coin_precisions[i]).toString()).toString(10);
+            var dx = cBN(Math.round(dx_ * coin_precisions[i]).toString()).toFixed(0,1);
             var dy_ = parseInt(await swap.methods.get_dy_underlying(i, j, dx).call()) / coin_precisions[j];
             var dy = dy_.toFixed(2);
             resolve([dy, dy_, dx_])
@@ -124,12 +124,12 @@ async function handle_trade() {
     if (b >= 0.001) {
         var dx = Math.floor($('#from_currency').val() * coin_precisions[i]);
         var min_dy = Math.floor($('#to_currency').val() * 0.95 * coin_precisions[j]);
-        dx = cBN(dx.toString()).toString(10);
+        dx = cBN(dx.toString()).toFixed(0,1);
         if ($('#inf-approval').prop('checked'))
             await ensure_underlying_allowance(i, max_allowance)
         else
             await ensure_underlying_allowance(i, dx);
-        min_dy = cBN(min_dy.toString()).toString(10);
+        min_dy = cBN(min_dy.toString()).toFixed(0,1);
         await swap.methods.exchange_underlying(i, j, dx, min_dy).send({
             from: default_account,
             gas: 1200000});
--- withdraw.js
+++ withdraw.js
@@ -97,17 +97,17 @@ async function handle_remove_liquidity() {
     var share_val = share.val();
     var amounts = $("[id^=currency_]").toArray().map(x => $(x).val());
     for (let i = 0; i < N_COINS; i++)
-        amounts[i] = cBN(Math.floor(amounts[i] / c_rates[i]).toString()).toString(10); // -> c-tokens
-    var min_amounts = amounts.map(x => cBN(Math.floor(0.97 * x).toString()).toString(10));
+        amounts[i] = cBN(Math.floor(amounts[i] / c_rates[i]).toString()).toFixed(0,1); // -> c-tokens
+    var min_amounts = amounts.map(x => cBN(Math.floor(0.97 * x).toString()).toFixed(0,1));
     var txhash;
     var default_account = (await web3.eth.getAccounts())[0];
     if (share_val == '---') {
         var token_amount = await swap.methods.calc_token_amount(amounts, false).call();
-        token_amount = cBN(Math.floor(token_amount * 1.01).toString()).toString(10)
+        token_amount = cBN(Math.floor(token_amount * 1.01).toString()).toFixed(0,1)
         await swap.methods.remove_liquidity_imbalance(amounts, token_amount).send({from: default_account, gas: 1000000});
     }
     else {
-        var amount = cBN(Math.floor(share_val / 100 * token_balance).toString()).toString(10);
+        var amount = cBN(Math.floor(share_val / 100 * token_balance).toString()).toFixed(0,1);
         if (share_val == 100)
             amount = await swap_token.methods.balanceOf(default_account).call();
         await swap.methods.remove_liquidity(amount, min_amounts).send({from: default_account, gas: 600000});
