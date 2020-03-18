08f321dbd5634818dc565bd74ddb048acef464af
AA <angelangelov97@gmail.com>
2020-03-18 18:03:45 +0200

    web3 rev update with file hash

--- common.js
+++ common.js
@@ -9,7 +9,7 @@ var c_rates = new Array(N_COINS);
 var fee;
 var admin_fee;
 
-var version = 17;
+var version = 18;
 localStorage.setItem('bUSDversion', version)
 
 var cBN = (val) => new BigNumber(val);
