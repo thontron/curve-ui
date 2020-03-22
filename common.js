210d042a6b594e4e6b067b168fe87c9864de7183
AA <angelangelov97@gmail.com>
2020-03-22 16:43:16 +0200

    bump versions

--- common.js
+++ common.js
@@ -9,7 +9,7 @@ var c_rates = new Array(N_COINS);
 var fee;
 var admin_fee;
 
-var version = 21;
+var version = 22;
 localStorage.setItem('version', version)
 
 var cBN = (val) => new BigNumber(val);
